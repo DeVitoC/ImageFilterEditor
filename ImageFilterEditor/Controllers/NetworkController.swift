42191ae4d8e47ede903b177b7b7199b6020b1b9e
Christopher DeVito <christopher.devito@protonmail.com>
2020-05-06 03:23:19 -0400

    Added Network Controller with method createAudioComment.

--- ImageFilterEditor/Controllers/NetworkController.swift
+++ ImageFilterEditor/Controllers/NetworkController.swift
@@ -9,5 +9,36 @@
 import Foundation
 
 class NetworkController {
+    let baseURL = URL(string: "https://lambdatimeline-9654e.firebaseio.com/")!
+    var audioComments: [AudioComment] = []
     
+    func createAudioComment(by author: String, storedAt url: URL, completion: @escaping () -> Void) {
+        
+        let comment = AudioComment(author: author, recording: url)
+        
+        audioComments.append(comment)
+        
+        let requestURL = baseURL.appendingPathComponent(comment.identifier).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(comment)
+        } catch {
+            NSLog("Error encoding audio comment to JSON: \(error)")
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error with message thread creation data task: \(error)")
+                completion()
+                return
+            }
+            
+            completion()
+        }.resume()
+    }
 }
